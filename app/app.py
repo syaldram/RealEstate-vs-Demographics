dd9b54be00aab273f5df2ac6a44b66a502b066bd
YRQV <yaldram_saad@bah.com>
2024-03-26 09:18:49 -0400

    Adding predictions

--- app/app.py
+++ app/app.py
@@ -62,9 +62,11 @@ state_abbreviations = {
 'Washington': 'WA',
 'West Virginia': 'WV',
 'Wisconsin': 'WI',
-'Wyoming': 'WY',
-'Puerto Rico': 'PR'
+'Wyoming': 'WY'
 }
+
+predictions = [
+    {'Alabama': 211079.27}, {'Alaska': 339052.03}, {'Arizona': 306941.34}, {'Arkansas': 194682.4}, {'California': 477853.56}, {'Colorado': 392629.2}, {'Connecticut': 353654.62}, {'Delaware': 292033.72}, {'District of Columbia': 536802.25}, {'Florida': 267907.0}, {'Georgia': 270794.62}, {'Hawaii': 558202.3}, {'Idaho': 306502.75}, {'Illinois': 261655.5}, {'Indiana': 204480.97}, {'Iowa': 213915.58}, {'Kansas': 240106.77}, {'Kentucky': 206727.23}, {'Louisiana': 229143.12}, {'Maine': 253239.69}, {'Maryland': 365915.56}, {'Massachusetts': 438043.97}, {'Michigan': 217066.14}, {'Minnesota': 295210.78}, {'Mississippi': 190834.75}, {'Missouri': 216126.39}, {'Montana': 288817.44}, {'Nebraska': 251140.03}, {'Nevada': 327448.84}, {'New Hampshire': 362251.78}, {'New Jersey': 416136.8}, {'New Mexico': 231550.6}, {'New York': 359355.4}, {'North Carolina': 240967.6}, {'North Dakota': 259075.05}, {'Ohio': 204287.16}, {'Oklahoma': 217383.16}, {'Oregon': 354209.8}, {'Pennsylvania': 240789.97}, {'Rhode Island': 340761.12}, {'South Carolina': 231672.92}, {'South Dakota': 250030.98}, {'Tennessee': 245226.92}, {'Texas': 269651.03}, {'Utah': 383908.1}, {'Vermont': 276144.6}, {'Virginia': 330968.97}, {'Washington': 422838.25}, {'West Virginia': 182807.48}, {'Wisconsin': 240014.98}, {'Wyoming': 263141.5}]
 
 @app.route("/")
 def home():
@@ -73,9 +75,15 @@ def home():
     chart_income_data = chart_income(state, year)
     make_map_data = make_map(year)
     chart_home_aff_data = chart_home_aff(year)
+
+    for states in predictions:
+        if state in states:
+            value = states[state]
+
     
     return render_template("index.html",
         footer_year = foot_year,
+        value = value,
         pop_pyramid=graph_pyramid(state, year), 
         state_abbreviations=state_abbreviations, 
         state=state, 
@@ -107,6 +115,10 @@ def state():
     if state is None:
         return "Invalid state abbreviation"
     
+    for data in predictions:
+        if state in data:
+            value = data[state]
+    
     year = '2022'
     chart_income_data = chart_income(state, year)
     make_map_data = make_map(year)
@@ -114,6 +126,7 @@ def state():
     
     return render_template("index.html",
         footer_year = foot_year,
+        value = value,
         pop_pyramid=graph_pyramid(state, year), 
         state_abbreviations=state_abbreviations, 
         state=state, 
@@ -138,6 +151,10 @@ def state():
         bedroom_size = bedroom_size(state, year),
         total_births = chart_births(),
         state_births = chart_state_births(state))
+
+@app.route('/predictions')
+def predictions():
+    return render_template('predictions.html',footer_year = foot_year)
 
 if __name__ == "__main__":
     app.run()
